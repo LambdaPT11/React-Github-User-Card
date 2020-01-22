28be89d8dbd59c2010287851ebc9ee4341fbe135
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-01-22 18:02:20 -0500

    added axios and class component

---
+++ github_user_card/src/components/userCard.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class UserCard extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            state: ''
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>mason roberts</h1>
+            </div>
+        );
+    }
+}
+
+export default UserCard;
